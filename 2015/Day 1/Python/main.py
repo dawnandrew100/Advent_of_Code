3bbb3b58945ff2dfd7b6fc7ebdef84553ebd5bc9
Andrew Hennis <81731313+dawnandrew100@users.noreply.github.com>
2024-07-24 17:40:57 -0400

    Functions make the world go round

--- 2015/Day 1/Python/main.py
+++ 2015/Day 1/Python/main.py
@@ -1,18 +1,26 @@
-with open("input.txt", "r") as file:
-  input = file.read()
+def main():
+  with open("input.txt", "r") as file:
+    input = file.read()
 
-floor = 0
-position = 0
-for char in input:
-  match char:
-    case "(":
-      floor +=1
-      position += 1
-    case ")":
-      floor -=1
-      position += 1
+  basement, floor = floor_finder(input)
+  print(f"Basement: {basement[0]}\nFinal floor: {floor}")
 
-  if floor == -1:
-    print(position)
+def floor_finder(contents):
+  floor = 0
+  position = 0
+  basement = []
+  for char in contents:
+    match char:
+      case "(":
+        floor +=1
+        position += 1
+      case ")":
+        floor -=1
+        position += 1
+  
+    if floor == -1:
+      basement.append(position)
+  return basement, floor
 
-print(floor)
+if __name__ == "__main__":
+  main()
