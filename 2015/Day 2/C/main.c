4f856e223088624b2a165617d6a9bf743984ec6b
Andrew Hennis <81731313+dawnandrew100@users.noreply.github.com>
2024-07-29 15:12:27 -0400

    Solution to Day 2 2015

--- 2015/Day 2/C/main.c
+++ 2015/Day 2/C/main.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include "fileio.h"
+#define input_size 1000
 
 struct box {
     int length;
     int width;
     int height;
 };
+
+int wrapping_paper_size(struct box *present_ptr);
+int ribbon_size(struct box *present_ptr);
 
 int main(void) { 
     char *filecon = (char*)malloc(MAX_DATA_SIZE*sizeof(char));
@@ -20,8 +24,8 @@ int main(void) {
         return status;
     }
     int n = 0;
-    char *token_array[1000];
-    struct box present_array[1000];
+    char *token_array[input_size];
+    struct box present_array[input_size];
     char *token = strtok(filecon, "\n");
    while(token != NULL) {
        token_array[n] = token;
@@ -53,9 +57,35 @@ int main(void) {
         }
     }
     free(filecon);
-    printf("%d*%d*%d\n", present_array[0].length,present_array[0].width,present_array[0].height);
-    printf("%d*%d*%d\n", present_array[1].length,present_array[1].width,present_array[1].height);
-    printf("%d*%d*%d\n", present_array[2].length,present_array[2].width,present_array[2].height);
-    printf("%d*%d*%d\n", present_array[3].length,present_array[3].width,present_array[3].height);
+    
+    int wrapping_paper_sqft = wrapping_paper_size(present_array);
+    int ribbon_ft = ribbon_size(present_array);
+    printf("Wraping paper needed: %d sqft\nRibbon needed: %d ft\n", wrapping_paper_sqft, ribbon_ft);
     return 0;    
+}
+
+int wrapping_paper_size(struct box *present_ptr) {
+    int total = 0;
+    int i;
+    for (i=0;i<input_size;i++) {
+        int side1 = 2 * present_ptr[i].length * present_ptr[i].width;
+        int side2 = 2 * present_ptr[i].width * present_ptr[i].height;
+        int side3 = 2 * present_ptr[i].height * present_ptr[i].length;
+        int box_array[3] = {side1, side2, side3};
+        int smallest_side = min(box_array, sizeof(box_array));
+        total += side1 + side2 + side3 + (smallest_side/2);
+    }
+    return total;
+}
+
+int ribbon_size(struct box *present_ptr) {
+    int i;
+    int total = 0;
+    for (i=0;i<input_size;i++) {
+        int perimeter[3] = {present_ptr[i].length, present_ptr[i].width, present_ptr[i].height};
+        int ribbon = (2*min(perimeter, sizeof(perimeter)))+(2*second_smallest(perimeter, sizeof(perimeter)));
+        int bow = present_ptr[i].length * present_ptr[i].width * present_ptr[i].height;
+        total += ribbon + bow;
+    }
+    return total;
 }
