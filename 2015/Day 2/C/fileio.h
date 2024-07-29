78398eb48db722a4848b336740f467deb3b06887
Andrew Hennis <81731313+dawnandrew100@users.noreply.github.com>
2024-07-29 15:11:54 -0400

    Added min and second smallest funcs
    min to find minimum value and second smallest to find the second smallest value

--- 2015/Day 2/C/fileio.h
+++ 2015/Day 2/C/fileio.h
@@ -3,10 +3,13 @@
 
 #define MAX_DATA_SIZE 10000
 
-int file_read(FILE* fileptr, char *filecon);
-int open_file(char *file_name, char *filecon);
+int file_read(FILE* fileptr, char *contents);
+int open_file(char *file_name, char *contents);
+int min(int *int_array, int array_size);
+int second_smallest(int *int_array, int array_size);
 
-int file_read(FILE* fileptr, char *filecon){
+
+int file_read(FILE* fileptr, char *contents){
     char data[MAX_DATA_SIZE]; 
     char buffer[10];
     char x[10];
@@ -25,14 +28,14 @@ int file_read(FILE* fileptr, char *filecon){
             strncat(data, buffer, strlen(buffer));
         }  
     } 
-    strncpy(filecon, data, MAX_DATA_SIZE);
+    strncpy(contents, data, MAX_DATA_SIZE);
     return 0;
 }
 
-int open_file(char *file_name, char *filecon){
+int open_file(char *file_name, char *contents){
     FILE *fptr = fopen(file_name, "r");
     if (fptr) {
-        int success = file_read(fptr, filecon);
+        int success = file_read(fptr, contents);
         
         if (fclose(fptr) == EOF) {
             perror("Error closing file");
@@ -42,5 +45,52 @@ int open_file(char *file_name, char *filecon){
     }
     perror("Error opening file");
     return 1;
+}
+
+int min(int *int_array, int array_size) {
+    if (array_size == 1) {
+        return int_array[0];
+    }
+    if (array_size == 2) {
+        int min = int_array[0];
+        if (int_array[1] < int_array[0]) {
+            min = int_array[1];
+        }
+        return min;
+    }
+    int i;
+    int min = int_array[0];
+    for (i=1;i<array_size;i++) {
+        if (int_array[i] < min) {
+            min = int_array[i];
+        }
+    }
+    return min;
+}
+
+int second_smallest(int *int_array, int array_size) {
+    array_size = array_size/sizeof(int);
+    if (array_size == 1) {
+        return int_array[0];
+    }
+    if (array_size == 2) {
+        int min2 = int_array[0];
+        if (int_array[1] > int_array[0]) {
+            min2 = int_array[1];
+        }
+        return min2;
+    }
+    int i;
+    int min = int_array[0];
+    int min2 = int_array[1];
+    for (i=1;i<array_size;i++) {
+        if (int_array[i] < min) {
+            min2 = min;
+            min = int_array[i];
+        } else if (int_array[i] >= min && int_array[i] <= min2) {
+            min2 = int_array[i];
+        }
+    }
+    return min2;
 }
 #endif
