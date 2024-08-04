d1daeaca7d7a9192c21214815eef3b4608b26cbe
Andrew Hennis <81731313+dawnandrew100@users.noreply.github.com>
2024-08-04 13:01:07 -0400

    2015 Day 8 solution

---
+++ 2015/Day 8/Python/main.py
@@ -0,0 +1,14 @@
+def main():
+  with open("input.txt", "r") as file:
+    contents = file.read().split("\n")
+  solution_1 = sum([len(content)-len(eval(content)) for content in contents])
+  solution_2 = 0
+  for content in contents:
+    content_plus = content.replace('\\','\\\\').replace('"','\\\"')
+    content_plus = f'\"{content_plus}\"'
+    solution_2 += len(content_plus) - len(content)
+  print(f"The difference using part one's rules is {solution_1} "
+        f"and the difference using part two's rules is {solution_2}!")
+
+if __name__ == "__main__":
+  main()
