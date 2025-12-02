3891dfd86aaf2e24b60ae44ac491cdbbb90b1dd9
Andrew Hennis <dawnandrew100@gmail.com>
2025-12-02 18:17:28 -0500

    update 2024 day 6
    This was from last year's AOC that was not completed

--- 2024/Day 06/Python/main.py
+++ 2024/Day 06/Python/main.py
@@ -1,8 +1,10 @@
 def main():
-    with open("..\\input.txt") as file:
-      inputs = []
-      for line in file.readlines():
-        inputs.append(line.strip())
+    with open("..\\test.txt") as file:
+        input = []
+        for line in file.readlines():
+            input.append(list(line.strip()))
+
+    print(input)
 
 if __name__ == "__main__":
     main()
